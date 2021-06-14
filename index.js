586266cbe93098978c5d5d37504b16cedebeaaee
Nhat Pham <pmnhat.17ck1@gmail.com>
2021-06-14 20:54:49 +0700

    fix

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const store = () => {
     return localStorage.getItem('arrSinhVien');
 }
 
-const store = () => {
-    console.log('abc');
-    return localStorage.getItem('store');
-}
-
 function B (){
     console.log('b edit index');
 }
